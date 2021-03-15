c01e604bcce2d032eec2573abb652d8624ead6dc
Francisco <franciscoparisalbero@gmail.com>
2021-03-15 14:24:03 +0100

    Ejercicios subidos a produccion

---
+++ docs/main.56db1803eef8484efe66.js
@@ -0,0 +1,36 @@
+(function(a){function b(d){if(c[d])return c[d].exports;var e=c[d]={i:d,l:!1,exports:{}};return a[d].call(e.exports,e,e.exports,b),e.l=!0,e.exports}var c={};return b.m=a,b.c=c,b.d=function(a,c,d){b.o(a,c)||Object.defineProperty(a,c,{enumerable:!0,get:d})},b.r=function(a){'undefined'!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(a,Symbol.toStringTag,{value:'Module'}),Object.defineProperty(a,'__esModule',{value:!0})},b.t=function(a,c){if(1&c&&(a=b(a)),8&c)return a;if(4&c&&'object'==typeof a&&a&&a.__esModule)return a;var d=Object.create(null);if(b.r(d),Object.defineProperty(d,'default',{enumerable:!0,value:a}),2&c&&'string'!=typeof a)for(var e in a)b.d(d,e,function(b){return a[b]}.bind(null,e));return d},b.n=function(a){var c=a&&a.__esModule?function(){return a['default']}:function(){return a};return b.d(c,'a',c),c},b.o=function(a,b){return Object.prototype.hasOwnProperty.call(a,b)},b.p='',b(b.s=0)})([function(a,b,c){'use strict';function d(){const a=`
+  <div>Añade el texto introducido en el input una vez pulsado el botón:</div>
+  <form>
+    <input type="text" id="texto" />
+    <button type="button" id="button_1">Añadir texto</button>
+  </form>
+
+  <div id="mostrar_texto"></div><hr>`,b=document.createElement('div');b.innerHTML=a,k.appendChild(b);let c=document.getElementById('texto'),d=document.getElementById('mostrar_texto');m=document.getElementById('button_1');let e=[];m.addEventListener('click',function(){e.push(c.value),d.innerHTML=e})}function e(){const a=`
+  <p>Crear dos input de números y un botón, al pulsar el botón, mostrar en un alert quien es el mayor de los dos.</p>
+  <form>
+      <input type="text" id="num1" style="margin-bottom: 20px";/>
+      <br>
+      <input type="text" id="num2" style="margin-bottom: 20px"/>
+      <button type="button" id="mayor">¿Quien es el mayor?</button>
+    </form>`,b=document.createElement('div');b.innerHTML=a,k.appendChild(b);let c=document.getElementById('mayor');c.addEventListener('click',function(){let a=parseInt(document.getElementById('num1').value),b=parseInt(document.getElementById('num2').value);a>b?alert(`${a} es mayor que ${b}`):b>a?alert(`${b} es mayor que ${a}`):a==b&&alert('Los dos n\xFAmeros son iguales'),a.isNaN||console.log(b)})}function f(){const a=`
+            <hr>
+            <p>Crear un botón que al pulsarlo genere una lista de 10 números aleatorios en un select vacio. 
+              Al pulsar de nuevo, se reseteara la lista.</p>
+            <button type="button" id="generarLista">Generar lista</button>
+              <select id="elementos"></select>
+            <hr>`,b=document.createElement('div');b.innerHTML=a,k.appendChild(b);const c=document.getElementById('generarLista');let d='';c.addEventListener('click',function(){let a=document.getElementById('elementos');for(let a=0;10>a;a++)d+='<option>'+g(1,100)+'</option>';a.innerHTML=d})}function g(a,b){return Math.floor(Math.random()*(b-a))+a}function h(){const a=`
+  <p>Dado un input y un boton, escribir nombres de personas (no controlamos nada de momento) 
+    y lo mostramos en una lista desordenada.</p>
+  <form>
+    <input id="nombrePersona" type="text" placeholder="Insertar nombre..." />
+    <button type="button" id="insertarNombre">Insertar</button>
+  </form>
+  <hr>`,b=document.createElement('div');b.innerHTML=a,k.appendChild(b);const c=document.getElementById('insertarNombre');c.addEventListener('click',function(){let a=document.getElementById('nombrePersona');if(''==a.value)alert('El nombre no puede estar vacio');else{const b=document.createElement('div');let c='<li> '+a.value+'</li>';b.innerHTML+=c,k.append(b)}})}function i(){const a=`
+              <p>Inserta una clave y vuelve a insertarla, si las dos son iguales... te sale un mensaje de alerta</p>
+              <input id="ingresarClave" placeholder="Ingresar clave...primero">
+              <button type="button" class="btn btn-primary btn-sm" id="botonClave">botonClave</button>
+              <input id="botonConfirmacion" placeholder="Ingresar clave de nuevo..." type="password">
+              <hr>`,b=document.createElement('div');b.style.display='visible',b.className='clave',b.id='claveId',b.innerHTML=a,k.append(b);const c=document.getElementById('botonClave'),d=document.getElementById('botonConfirmacion');c.addEventListener('click',function(){const a=document.getElementById('ingresarClave'),b=a.value;a.value='';let c=d.value;console.log(c),c==b&&alert('La contrase\xF1a primera y segunda son correctas.'),d.value=''})}function j(){const a=`<p>Incluimos un nuevo botón, en el que oculta el div anterior al pulsar el input de tipo botón: </p>
+  <p>Si volvemos a clickar de nuevo, volverremos a mostrar el div anterior. </p>
+  <input id="botonOcultar" type=button value='clickaquí para ocultar el div'>
+  <hr>`,b=document.createElement('div');b.innerHTML=a,k.append(b);const c=document.getElementById('botonOcultar');c.addEventListener('click',function(){const a=document.getElementById('claveId');a.style.visibility='visible'==a.style.visibility?'hidden':'visible'})}c.r(b);const k=document.body;let l,m;const n=()=>{l=document.querySelectorAll('input'),l.forEach(function(a){a.addEventListener('click',async()=>{console.log(a),console.log('item.value: '+a.value),console.log('item.id: '+a.id),k.style.backgroundColor='opcion1'==a.value?'antiquewhite':'opcion2'==a.value?'beige':'white'})})};(()=>{n(),d(),e(),f(),h(),i(),j()})()}]);
--- src/js/ejercicioPruebas.js
+++ src/js/ejercicioPruebas.js
@@ -1,26 +1,9 @@
 const body = document.body;
-let opcion;
+let opcion, boton_1;
 
-const checkOpcion = () => {
-  const html = `<input type="radio" name="eleccion" value="opcion1">Opcion1<br/>
-    <input type="radio" name="eleccion" value="opcion2" >Opcion2<br/>
-    <input type="radio" name="eleccion" value="opcion3">Opcion3<br/>`;
+// +++++++++++++++++++++++++++++++++++++++++++++++++
 
-  const div = document.createElement("div");
-  div.innerHTML = html;
-  body.append(div);
-};
-
-function muestraOpcion() {
-  var opciones = document.getElementsByName("eleccion");
-  console.log(opciones);
-  for (let i = 0; i < opciones.length; i++) {
-    if (opciones[i].checked) {
-      alert(opciones[i].value);
-    }
-  }
-}
-
+// Cambia de color de la página, dependiendo de que checkboxs pulses
 const elegido = () => {
   opcion = document.querySelectorAll("input");
 
@@ -29,12 +12,11 @@ const elegido = () => {
       console.log(item);
       console.log("item.value: " + item.value);
       console.log("item.id: " + item.id);
-      //alert("Has pulsado la: " + item.value);
+
       if (item.value == "opcion1") {
-        console.log("opcion1 pulsada");
-        body.style.backgroundColor = "red";
+        body.style.backgroundColor = "antiquewhite";
       } else if (item.value == "opcion2") {
-        body.style.backgroundColor = "green";
+        body.style.backgroundColor = "beige";
       } else {
         body.style.backgroundColor = "white";
       }
@@ -42,8 +24,211 @@ const elegido = () => {
   });
 };
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// crear un input y un button, cuando le demos al boton,
+//  se añadira el texto que hemos puesto en el input en un div vacio.
+// Recuerda se añadirá el texto, no se machaca lo que haya en el div.
+function afegirText() {
+  const html = `
+  <div>Añade el texto introducido en el input una vez pulsado el botón:</div>
+  <form>
+    <input type="text" id="texto" />
+    <button type="button" id="button_1">Añadir texto</button>
+  </form>
+
+  <div id="mostrar_texto"></div><hr>`;
+
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  body.appendChild(div);
+
+  let texto = document.getElementById("texto");
+  let div_2 = document.getElementById("mostrar_texto");
+  boton_1 = document.getElementById("button_1");
+  let textoAnadir = [];
+
+  boton_1.addEventListener("click", function () {
+    // div_2.innerHTML = texto += texto.value;
+    textoAnadir.push(texto.value);
+    div_2.innerHTML = textoAnadir;
+  });
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Crear dos input de números y un botón, al pulsar el botón, mostrar en un
+// alert quien es el mayor de los dos.
+function mayorNum() {
+  const html = `
+  <p>Crear dos input de números y un botón, al pulsar el botón, mostrar en un alert quien es el mayor de los dos.</p>
+  <form>
+      <input type="text" id="num1" style="margin-bottom: 20px";/>
+      <br>
+      <input type="text" id="num2" style="margin-bottom: 20px"/>
+      <button type="button" id="mayor">¿Quien es el mayor?</button>
+    </form>`;
+
+  const div = document.createElement("div");
+  div.innerHTML = html;
+
+  body.appendChild(div);
+
+  let boton_2 = document.getElementById("mayor");
+
+  //Averiguem quin es major
+  boton_2.addEventListener("click", function () {
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+
+    if (num1 > num2) {
+      alert(`${num1} es mayor que ${num2}`);
+    } else if (num2 > num1) {
+      alert(`${num2} es mayor que ${num1}`);
+    } else if (num1 == num2) {
+      alert("Los dos números son iguales");
+    }
+    if (!num1.isNaN) {
+      console.log(num2);
+    }
+  });
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Crear un botón que al pulsarlo genere una lista de 10 números
+// aleatorios en un select vacio. Al pulsar de nuevo, se reseteara la lista.
+function generaLista() {
+  const html = `
+            <hr>
+            <p>Crear un botón que al pulsarlo genere una lista de 10 números aleatorios en un select vacio. 
+              Al pulsar de nuevo, se reseteara la lista.</p>
+            <button type="button" id="generarLista">Generar lista</button>
+              <select id="elementos"></select>
+            <hr>`;
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  body.appendChild(div);
+
+  const boton_3 = document.getElementById("generarLista");
+  let cadaElemento = "";
+
+  boton_3.addEventListener("click", function () {
+    let elementos = document.getElementById("elementos");
+    for (let i = 0; i < 10; i++) {
+      cadaElemento += "<option>" + getRandomInt(1, 100) + "</option>";
+    }
+    elementos.innerHTML = cadaElemento;
+  });
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Dado un input y un boton, escribir nombres de personas (no controlamos nada de momento)
+//  y lo mostramos en una lista desordenada.
+// <button onclick="insertarNombre(event)">Insertar</button>
+
+function listaDesordenada() {
+  const html = `
+  <p>Dado un input y un boton, escribir nombres de personas (no controlamos nada de momento) 
+    y lo mostramos en una lista desordenada.</p>
+  <form>
+    <input id="nombrePersona" type="text" placeholder="Insertar nombre..." />
+    <button type="button" id="insertarNombre">Insertar</button>
+  </form>
+  <hr>`;
+
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  body.appendChild(div);
+
+  const boton_4 = document.getElementById("insertarNombre");
+  boton_4.addEventListener("click", function () {
+    let nombre = document.getElementById("nombrePersona");
+    if (nombre.value == "") {
+      alert("El nombre no puede estar vacio");
+    } else {
+      const lista = document.createElement("div");
+      let opcion = "<li> " + nombre.value + "</li>";
+
+      lista.innerHTML += opcion;
+      body.append(lista);
+    }
+  });
+}
+
+function claveContrasenya() {
+  const html = `
+              <p>Inserta una clave y vuelve a insertarla, si las dos son iguales... te sale un mensaje de alerta</p>
+              <input id="ingresarClave" placeholder="Ingresar clave...primero">
+              <button type="button" class="btn btn-primary btn-sm" id="botonClave">botonClave</button>
+              <input id="botonConfirmacion" placeholder="Ingresar clave de nuevo..." type="password">
+              <hr>`;
+  const div = document.createElement("div");
+  div.style.display = "visible";
+  div.className = "clave";
+  div.id = "claveId";
+  div.innerHTML = html;
+  body.append(div);
+
+  const botonClave = document.getElementById("botonClave");
+  const confirmacion = document.getElementById("botonConfirmacion");
+
+  // const valor;
+
+  botonClave.addEventListener("click", function () {
+    const ingresar = document.getElementById("ingresarClave");
+    const valor = ingresar.value;
+    ingresar.value = "";
+
+    let resultado = confirmacion.value;
+    // confirmacion.placeholder = "";
+    console.log(resultado);
+
+    if (resultado == valor) {
+      alert("La contraseña primera y segunda son correctas.");
+    }
+    confirmacion.value = "";
+  });
+}
+
+// <input type=button onClick="location.href='index_2.html'"
+
+function ocultar() {
+  const html = `<p>Incluimos un nuevo botón, en el que oculta el div anterior al pulsar el input de tipo botón: </p>
+  <p>Si volvemos a clickar de nuevo, volverremos a mostrar el div anterior. </p>
+  <input id="botonOcultar" type=button value='clickaquí para ocultar el div'>
+  <hr>`;
+
+  const div = document.createElement("div");
+  div.innerHTML = html;
+  body.append(div);
+
+  const clickOcultar = document.getElementById("botonOcultar");
+  clickOcultar.addEventListener("click", function () {
+    const divaOcultar = document.getElementById("claveId");
+    // console.log(divaOcultar.style.visibility == "visible");
+
+    if (divaOcultar.style.visibility == "visible") {
+      divaOcultar.style.visibility = "hidden";
+    } else {
+      divaOcultar.style.visibility = "visible";
+    }
+  });
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++
+
 export const init = () => {
-  //   checkOpcion();
-  muestraOpcion();
   elegido();
+  afegirText();
+  mayorNum();
+  generaLista();
+  listaDesordenada();
+  claveContrasenya();
+  ocultar();
 };
